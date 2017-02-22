20c9019beeed1ff39ae2b3991894bd7c485dece9
njaramillo-ng <njaramillo@nemorisgames.com>
2017-02-22 18:22:02 -0300

    Pulso periodico

--- EcografiaDoppler/Assets/Scripts/TestGraph.cs
+++ EcografiaDoppler/Assets/Scripts/TestGraph.cs
@@ -15,14 +15,16 @@ public class TestGraph : MonoBehaviour {
 		for (int i = 0; i < sizeScreen; i++) {
 			texture.SetPixel(indiceActual % sizeScreen, i, Color.black);
 		}
-		float f0 = Mathf.Sin (1.333f * indiceActual * 360f / sizeScreen * Mathf.PI / 180f);
+		float aux = 1f;
+		//float f0 = Mathf.Sin (1.333f * indiceActual * 360f / sizeScreen * Mathf.PI / 180f);
+		float test = aux*(Mathf.Sin (0.5f * indiceActual * 360f / sizeScreen * Mathf.PI/180f * Mathf.PI/0.5f - 0.8f + Mathf.Sin(0.5f*indiceActual* 360f / sizeScreen * Mathf.PI/180f * Mathf.PI/0.5f))* Mathf.Log (indiceActual) + 1.8f);
 		for (int i = 0; i < Random.Range (5, 15); i++) {
-			texture.SetPixel(indiceActual % sizeScreen, Random.Range(-20, -1) + sizeScreen / 2 + (int)(10f * Mathf.Sin(1.333f * indiceActual * 360f / sizeScreen  * Mathf.PI / 180f)), Color.blue);
+			texture.SetPixel(indiceActual % sizeScreen, Random.Range(-20, -1) + sizeScreen / 2 + (int)(5f * test), Color.blue);
 		}
 		for (int i = 0; i < Random.Range (5, 10); i++) {
-			texture.SetPixel(indiceActual % sizeScreen, Random.Range(-40, -1) + sizeScreen / 2 + (int)(10f * Mathf.Sin(1.333f * indiceActual * 360f / sizeScreen  * Mathf.PI / 180f)), Color.red);
+			texture.SetPixel(indiceActual % sizeScreen, Random.Range(-40, -1) + sizeScreen / 2 + (int)(5f * test), Color.red);
 		}
-		texture.SetPixel(indiceActual % sizeScreen, sizeScreen / 2 + (int)(10f * f0), Color.white);
+		texture.SetPixel(indiceActual % sizeScreen, sizeScreen / 2 + (int)(5f * test), Color.white);
 		texture.Apply();
 		indiceActual++;
 	}
