1f362caab476739f3a5873501d7bc42a0f572a84
nemorisgames <contacto@nemorisgames.com>
2017-04-25 16:38:16 -0300

    individual window

---
+++ EcografiaDoppler/Assets/Scripts/LabelIndicators.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LabelIndicators : MonoBehaviour {
+	public UILabel verticalLabel;
+	public UILabel horizontalLabel;
+	public int verticalScale = 0;
+	public int verticalPosition = 1;
+	public int horizontalScale = 0;
+	// Use this for initialization
+	void Start (){
+		verticalLabel.text = "";
+		for(int i = 0; i <= 17; i++){
+			verticalLabel.text += "" + ((verticalPosition - i) * 100) + "\n\n"; 
+		}
+	}
+
+	public void changeVerticalPosition(int v){
+		verticalPosition += v;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
