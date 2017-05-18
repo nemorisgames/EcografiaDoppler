c752545691e01130ab96aeb9fc7aac914f171817
nemorisgames <contacto@nemorisgames.com>
2017-05-18 11:35:45 -0400

    nueva grafica

---
+++ EcografiaDoppler/Assets/Scripts/ComportamientoCursor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComportamientoCursor : MonoBehaviour {
+    public Texture2D durezas;
+    public Vector2 sizeDurezas;
+	// Use this for initialization
+	void Start () {
+		
+	}
+    
+    public void clickOnImage()
+    {
+        Vector2 posicionClick = new Vector2((Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position)).x + sizeDurezas.x/2f, (Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position)).y + sizeDurezas.y / 2f);
+        print(posicionClick);
+    }
+
+    void OnMouseDown()
+    {
+        print("aqui");
+    }
+
+    // Update is called once per frame
+    void Update () {
+        
+    }
+}
